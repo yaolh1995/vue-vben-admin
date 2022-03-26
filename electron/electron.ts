f133d763f511f438628d118bbd5457d28becf612
姚霖宏 <7881785+ylh1995@user.noreply.gitee.com>
2022-03-26 21:10:40 +0800

    feat: support electron

---
+++ electron/electron.ts
@@ -0,0 +1,51 @@
+// electron/electron.ts
+const path = require('path');
+import { app, BrowserWindow } from 'electron';
+
+const isDev = process.env.NODE_ENV == 'true' ? true : false;
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.ts'),
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
+    isDev ? 'https://localhost:3100/' : `file://${path.join(__dirname, '../dist/index.html')}`,
+  );
+  console.log(`file://${path.join(__dirname, '../dist/index.html')}`, 'fuck');
+
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', function () {
+    app.setBadgeCount(1);
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
